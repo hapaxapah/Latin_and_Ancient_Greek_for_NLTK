d84a461c2b81175a0dfed2c75b81313e704ee927
Kyle Johnson <kyle@localhost.(none)>
2013-03-02 21:22:14 -0500

    added j and v replacer

---
+++ lemmata/j_and_v_converter.py
@@ -0,0 +1,14 @@
+import re
+replacement_patterns = [
+    (r'j', 'i'),
+    (r'v', 'u'),
+]
+
+class JVReplacer(object):
+    def __init__(self, patterns=replacement_patterns):
+        self.patterns = [(re.compile(regex), repl) for (regex, repl) in patterns]
+    def replace(self, text):
+        s = text
+        for (pattern, repl) in self.patterns:
+            (s, count) = re.subn(pattern, repl, s)
+        return s
